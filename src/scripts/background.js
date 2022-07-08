c8b02300debbf64d548835ed68bc290260848937
Eduardo Queiroz <69441532+eduardoqsilva@users.noreply.github.com>
2022-07-07 23:49:17 -0300

    update background images

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -74,7 +74,7 @@ function backgroundOfTheDay() {
     //let cpr = document.getElementById('footer')
 
 
-    const url = `https://api.pexels.com/v1/search?query=${themes()}+${dayOrNigth()}&page=${rdm(6)}&orientation=landscape` 
+    const url = `https://api.pexels.com/v1/search?query=${themes()}+${dayOrNigth()}&page=${rdm(6)}&orientation=landscape&size=large` 
 
     console.log(url)
     fetch(url, {
